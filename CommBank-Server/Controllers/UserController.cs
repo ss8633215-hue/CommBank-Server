bea14412b63f9b2accba929539f3149035d1ce7e
ss8633215-hue <ss8633215@gmail.com>
2026-06-12 12:56:35 +0530

    Update UserController

--- CommBank-Server/Controllers/UserController.cs
+++ CommBank-Server/Controllers/UserController.cs
@@ -10,8 +10,10 @@ public class UserController : ControllerBase
 {
     private readonly IUsersService _usersService;
 
-    public UserController(IUsersService usersService) =>
+    public UserController(IUsersService usersService)
+    {
         _usersService = usersService;
+    }
 
     [HttpGet]
     public async Task<List<User>> Get() =>
@@ -24,6 +26,52 @@ public class UserController : ControllerBase
 
         if (user is null)
         {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(User newUser)
+    {
+        await _usersService.CreateAsync(newUser);
+
+        return CreatedAtAction(nameof(Get), new { id = newUser.Id }, newUser);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, User updatedUser)
+    {
+        var user = await _usersService.GetAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        updatedUser.Id = user.Id;
+
+        await _usersService.UpdateAsync(id, updatedUser);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var user = await _usersService.GetAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        await _usersService.RemoveAsync(id);
+
+        return NoContent();
+    }
+}
             return NotFound();
         }
 
