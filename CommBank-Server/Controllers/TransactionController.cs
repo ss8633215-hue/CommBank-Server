c8e27d651a857ba80f48058162865da711a4e88c
ss8633215-hue <ss8633215@gmail.com>
2026-06-12 12:57:29 +0530

    Update TransactionController

--- CommBank-Server/Controllers/TransactionController.cs
+++ CommBank-Server/Controllers/TransactionController.cs
@@ -19,11 +19,61 @@ public class TransactionController : ControllerBase
 
     [HttpGet("User/{id:length(24)}")]
     public async Task<List<Transaction>?> GetForUser(string id) =>
-    await _transactionsService.GetForUserAsync(id);
+        await _transactionsService.GetForUserAsync(id);
 
     [HttpGet("{id:length(24)}")]
     public async Task<ActionResult<Transaction>> Get(string id)
     {
+        var transaction = await _transactionsService.GetAsync(id);
+
+        if (transaction is null)
+        {
+            return NotFound();
+        }
+
+        return transaction;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Transaction newTransaction)
+    {
+        await _transactionsService.CreateAsync(newTransaction);
+
+        return CreatedAtAction(nameof(Get), new { id = newTransaction.Id }, newTransaction);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Transaction updatedTransaction)
+    {
+        var transaction = await _transactionsService.GetAsync(id);
+
+        if (transaction is null)
+        {
+            return NotFound();
+        }
+
+        updatedTransaction.Id = transaction.Id;
+
+        await _transactionsService.UpdateAsync(id, updatedTransaction);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var transaction = await _transactionsService.GetAsync(id);
+
+        if (transaction is null)
+        {
+            return NotFound();
+        }
+
+        await _transactionsService.RemoveAsync(id);
+
+        return NoContent();
+    }
+}
         var transaction = await _transactionsService.GetAsync(id);
 
         if (transaction is null)
