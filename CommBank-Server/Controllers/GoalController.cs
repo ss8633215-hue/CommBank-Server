5d0b1e92550752b26557cfbae2129037dd4632d5
ss8633215-hue <ss8633215@gmail.com>
2026-06-12 12:54:04 +0530

    Implement Goal Controller CRUD operations

--- CommBank-Server/Controllers/GoalController.cs
+++ CommBank-Server/Controllers/GoalController.cs
@@ -9,12 +9,10 @@ namespace CommBank.Controllers;
 public class GoalController : ControllerBase
 {
     private readonly IGoalsService _goalsService;
-    private readonly IUsersService _usersService;
 
-    public GoalController(IGoalsService goalsService, IUsersService usersService)
+    public GoalController(IGoalsService goalsService)
     {
         _goalsService = goalsService;
-        _usersService = usersService;
     }
 
     [HttpGet]
@@ -24,6 +22,56 @@ public class GoalController : ControllerBase
     [HttpGet("{id:length(24)}")]
     public async Task<ActionResult<Goal>> Get(string id)
     {
+        var goal = await _goalsService.GetAsync(id);
+
+        if (goal is null)
+        {
+            return NotFound();
+        }
+
+        return goal;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(Goal newGoal)
+    {
+        await _goalsService.CreateAsync(newGoal);
+
+        return CreatedAtAction(nameof(Get), new { id = newGoal.Id }, newGoal);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Goal updatedGoal)
+    {
+        var goal = await _goalsService.GetAsync(id);
+
+        if (goal is null)
+        {
+            return NotFound();
+        }
+
+        updatedGoal.Id = goal.Id;
+
+        await _goalsService.UpdateAsync(id, updatedGoal);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var goal = await _goalsService.GetAsync(id);
+
+        if (goal is null)
+        {
+            return NotFound();
+        }
+
+        await _goalsService.RemoveAsync(id);
+
+        return NoContent();
+    }
+}
         var goal = await _goalsService.GetAsync(id);
 
         if (goal is null)
